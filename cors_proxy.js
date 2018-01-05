90f36068f547eb0f0320e8754f1414b300086e73
allthingsclowd <allthingsclowd@users.noreply.github.com>
2018-01-05 13:12:26 +0000

    Successfully enabled SSL options

--- cors_proxy.js
+++ cors_proxy.js
@@ -9,7 +9,16 @@ var host = process.env.HOST || '0.0.0.0';
 var port = process.env.PORT || 2337;
  
 var cors_proxy = require('cors-anywhere');
+var fs = require('fs');
+var path = require('path');
 
-cors_proxy.createServer({ originWhitelist: [],})
+// read in the self-signed certs created with the following command:
+// openssl req -newkey rsa:2048 -nodes -x509 -days 99999 -out althingscloud.pem -keyout corsproxyprivate.pem
+var httpsOptions = {
+    key: fs.readFileSync('certs/corsproxyprivate.pem')
+  , cert: fs.readFileSync('certs/althingscloud.pem')
+  };
+
+cors_proxy.createServer({ originWhitelist: [],httpsOptions,})
             .listen(port, host, function() { console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
